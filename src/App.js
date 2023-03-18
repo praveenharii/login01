4c3398019bff81fd941c422ddc14fdfe52806755
praveenharii <119642005+praveenharii@users.noreply.github.com>
2023-03-19 04:09:27 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,153 @@
+import React, {Component} from 'react';
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+import SignUpForm from "./pages/SignUpForm";
+import SignInForm from "./pages/SignInForm";
+require('./App.css');
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router basename="/react-auth-ui/">
+        <div className="App">
+          <div className="appAside" />
+          <div className="appForm">
+            <div className="pageSwitcher">
+              <NavLink
+                to="/sign-in"
+                activeClassName="pageSwitcherItem-active"
+                className="pageSwitcherItem"
+              >
+                Sign In
+              </NavLink>
+              <NavLink
+                exact
+                to="/"
+                activeClassName="pageSwitcherItem-active"
+                className="pageSwitcherItem"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+
+            <div className="formTitle">
+              <NavLink
+                to="/sign-in"
+                activeClassName="formTitleLink-active"
+                className="formTitleLink"
+              >
+                Sign In
+              </NavLink>{" "}
+              or{" "}
+              <NavLink
+                exact
+                to="/"
+                activeClassName="formTitleLink-active"
+                className="formTitleLink"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+
+            <Route exact path="/" component={SignUpForm} />
+            <Route path="/sign-in" component={SignInForm} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+  
+//   return (
+//     <form>
+//       <div className="input-container">
+//         <label>Username </label>
+//         <input type="text" name="uname" required />
+       
+//       </div>
+//       <div className="input-container">
+//         <label>Password </label>
+//         <input type="password" name="pass" required />
+       
+//       </div>
+//       <div className="button-container">
+//         <input type="submit" />
+//       </div>
+      
+//     </form>
+//   );
+// };
+
+// export default App;
+
+
+
+
+/*{ <div className="container">
+  <h2>Sign up</h2>
+
+  <input
+    type="text"
+    placeholder="E-mail"
+    name="email"
+    onChange={this.handleInputChange}
+    required
+  />
+
+  <input
+    type="password"
+    placeholder={"Password"}
+    name="password"
+    onChange={this.handleInputChange}
+    required
+  />
+  {"" && <p className="error">{ }</p>}
+  <button id="login-button" type="submit">
+    Login
+  </button>
+
+  <p>Existing user</p>
+  <p>Signup</p>
+</div>} */
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+
+import "./index.css"
+import SignInForm from './pages/SignInForm';
+import SignUpForm from './pages/SignUpForm';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+ 
   <React.StrictMode>
-    <App />
+    <SignUpForm />
   </React.StrictMode>
+
 );
 
 
 
+ReactDOM.render(<App />, document.getElementById("root"));
